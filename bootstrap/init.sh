c266bcd3829cb0d8946f70a63398863de6c28a84
Kaj Fehlhaber <kaj.fehlhaber@gmail.com>
2025-02-11 22:22:19 +0100

    fix: all the dirs

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -3,6 +3,6 @@
 helm dependency build ./charts/01
 helm dependency build ./charts/02
 
-helm install first ./01
-helm install second ./02
+helm install first ./charts/01
+helm install second ./charts/02
 
