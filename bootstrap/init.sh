980e22ea1e2c55d75ba8de31e605050fa5337928
Kaj Fehlhaber <kaj.fehlhaber@gmail.com>
2025-02-27 23:40:13 +0100

    feat: install Cilium in bootstrap

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 
-helm repo add https://argoproj.github.io/argo-helm
+kind delete cluster
+systemd-run --scope --user kind create cluster --config=kind-config.yaml
+cilium install --version 1.17.1
+
+helm repo add argo https://argoproj.github.io/argo-helm
 helm dependency build ./charts/01
 helm dependency build ./charts/02
 
