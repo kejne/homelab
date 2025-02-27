b08ba40eace9b24e375d75e3c7b60293af7b2a8d
Kaj Fehlhaber <kaj.fehlhaber@gmail.com>
2025-02-27 23:17:25 +0100

    refactor: streamline bootstrap

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
+helm repo add https://argoproj.github.io/argo-helm
 helm dependency build ./charts/01
 helm dependency build ./charts/02
 
-helm install first ./charts/01
-helm install second ./charts/02
+helm install first ./charts/01 -n argocd --create-namespace
+helm install second ./charts/02 -n argocd
 
